cfa06eec105537df464c220024aa7e0e7dcbe8f2
richardmarques <richardmarques@localhost>
2020-02-11 15:19:34 -0200

    versao inicial Conta

---
+++ Conta/src/test/java/com/gm4c/conta/ContaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gm4c.conta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
