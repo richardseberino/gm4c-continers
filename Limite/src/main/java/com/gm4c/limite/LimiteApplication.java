d2d92c61cc14920ce2958446d55fdf68bb9f0aff
richardmarques <richardmarques@localhost>
2020-02-11 15:18:33 -0200

    versao inicial limite

---
+++ Limite/src/main/java/com/gm4c/limite/LimiteApplication.java
@@ -0,0 +1,13 @@
+package com.gm4c.limite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LimiteApplication.class, args);
+	}
+
+}
---
+++ Limite/src/test/java/com/gm4c/limite/LimiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gm4c.limite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
