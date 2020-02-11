ded7b80097842dd42097f03177f650316d639e1e
richardmarques <richardmarques@localhost>
2020-02-11 15:14:02 -0200

    versao inicial

---
+++ Senha/src/main/java/com/gm4c/senha/SenhaApplication.java
@@ -0,0 +1,13 @@
+package com.gm4c.senha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SenhaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SenhaApplication.class, args);
+	}
+
+}
---
+++ Senha/src/test/java/com/gm4c/senha/SenhaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gm4c.senha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SenhaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
