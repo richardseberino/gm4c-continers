7a2f59316461a1cfe3642ced2d891db57207ff63
richardmarques <richardmarques@localhost>
2020-02-11 15:17:16 -0200

    versao inicial da TEF

---
+++ TEF/src/main/java/com/gm4c/tef/TefApplication.java
@@ -0,0 +1,13 @@
+package com.gm4c.tef;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TefApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TefApplication.class, args);
+	}
+
+}
---
+++ TEF/src/test/java/com/gm4c/tef/TefApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gm4c.tef;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TefApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
