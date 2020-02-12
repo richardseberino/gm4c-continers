870ebef486e342ae1c4ff441d2442cfd5cc1ce54
Marcelo Alvaro de Araujo <malvaro@br.ibm.com>
2020-02-12 18:55:30 -0300

    Alteraçao servico Limite

---
+++ Limite/src/main/java/com/gm4c/limite/dto/LimiteRepositorio.java
@@ -0,0 +1,13 @@
+package com.gm4c.limite.dto;
+
+import java.util.List;
+
+import org.springframework.data.cassandra.repository.CassandraRepository;
+import org.springframework.data.cassandra.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface LimiteRepositorio extends CassandraRepository<LimiteDto, String>{
+	@Query("SELECT id_limite, agencia, conta, dv, valor_limite, valor_utilizado from limite where agencia = :agencia and conta = :conta and dv = :dv ALLOW FILTERING")
+	List<LimiteDto> pesquisaLimite(@Param("agencia") int agencia, @Param("conta") int conta, @Param("dv") int dv, @Param("valor_limite") double valor_limite, @Param("valor_utilizado") double valor_utilizado);
+
+}
